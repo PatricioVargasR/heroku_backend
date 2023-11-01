88fb80f62579c92d3b9629190bef994247fb8132
lPatricio_VargasR <1722110158@utectulancingo.edu.mx>
2023-11-01 09:34:47 -0600

    UPDATE

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ app = fastapi.FastAPI()
 
 # Permitimos los origenes para conectarse
 origins = [
-    "http://localhost:8080",
+    "http://localhost:8080"
     "https://herokufrontendsql-8c522739b4c3.herokuapp.com"
 ]
 
@@ -38,6 +38,15 @@ def inicio():
 
 # Rutas para las operaciones CRUD
 
+@app.post("/contactos")
+async def crear_contacto(contacto: Contacto):
+    """Crea un nuevo contacto."""
+    # TODO Inserta el contacto en la base de datos y responde con un mensaje
+    c = conn.cursor()
+    c.execute('INSERT INTO contactos (email, nombre, telefono) VALUES (?, ?, ?)',
+              (contacto.email, contacto.nombre, contacto.telefono))
+    conn.commit()
+    return contacto
 
 @app.get("/contactos")
 async def obtener_contactos():
@@ -50,3 +59,34 @@ async def obtener_contactos():
         contacto = {"email":row[0],"nombre":row[1], "telefono":row[2]}
         response.append(contacto)
     return response
+
+
+@app.get("/contactos/{email}")
+async def obtener_contacto(email: str):
+    """Obtiene un contacto por su email."""
+    # Consulta el contacto por su email
+    c = conn.cursor()
+    c.execute('SELECT * FROM contactos WHERE email = ?', (email,))
+    contacto = None
+    for row in c:
+        contacto = {"email":row[0],"nombre":row[1],"telefono":row[2]}
+    return contacto
+
+
+@app.put("/contactos/{email}")
+async def actualizar_contacto(email: str, contacto: Contacto):
+    """Actualiza un contacto."""
+    c = conn.cursor()
+    c.execute('UPDATE contactos SET nombre = ?, telefono = ? WHERE email = ?',
+              (contacto.nombre, contacto.telefono, email))
+    conn.commit()
+    return contacto
+
+@app.delete("/contactos/{email}")
+async def eliminar_contacto(email: str):
+    """Elimina un contacto."""
+    # TODO Elimina el contacto de la base de datos
+    c = conn.cursor()
+    c.execute('DELETE FROM contactos WHERE email = ?', (email,))
+    conn.commit()
+    return {"mensaje":"Contacto eliminado"}
